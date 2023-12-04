d1ae2b55a8ede70a9a1476004db5897670ba593b
husnasf <151705309+husnasf@users.noreply.github.com>
2023-12-04 15:42:38 +0800

    Add files via upload

---
+++ RM1campaign.js
@@ -0,0 +1,43 @@
+document.getElementById("campaign").addEventListener("mouseover", campaignMouseOver);
+document.getElementById("campaign").addEventListener("mouseout", campaignMouseOut);
+    
+    function campaignMouseOver() {
+      document.getElementById("campaign").style.color = "rgb(123, 102, 255)";
+    }
+    
+    function campaignMouseOut() {
+      document.getElementById("campaign").style.color = "rgb(17, 63, 103)";
+    }
+
+document.getElementById("tagline").addEventListener("mouseover", taglineMouseOver);
+document.getElementById("tagline").addEventListener("mouseout", taglineMouseOut);
+        
+    function taglineMouseOver() {
+    document.getElementById("tagline").style.color = "rgb(123, 102, 255)";
+    }
+        
+    function taglineMouseOut() {
+    document.getElementById("tagline").style.color = "rgb(17, 63, 103";
+    }
+
+document.getElementById("verse1").addEventListener("mouseover", verse1MouseOver);
+document.getElementById("verse1").addEventListener("mouseout", verse1MouseOut);
+                
+    function verse1MouseOver() {
+    document.getElementById("verse1").style.color = "rgb(123, 102, 255)";
+    }
+                
+    function verse1MouseOut() {
+    document.getElementById("verse1").style.color = "rgb(17, 63, 103)";
+    }
+            
+document.getElementById("verse2").addEventListener("mouseover", verse2MouseOver);
+document.getElementById("verse2").addEventListener("mouseout", verse2MouseOut);
+                                
+    function verse2MouseOver() {
+    document.getElementById("verse2").style.color = "rgb(123, 102, 255)";
+    }
+                                
+    function verse2MouseOut() {
+    document.getElementById("verse2").style.color = "rgb(17, 63, 103)";
+    }
